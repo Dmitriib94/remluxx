057446768fb625350694a7782e1dd47fb6aea5f6
Dmitriib94 <dmitriib94@yandex.ru>
2026-05-27 00:02:23 +0800

    Delete utils.js

--- js/utils.js
+++
@@ -1,3 +0,0 @@
-export const $ = (selector, ctx = document) => ctx.querySelector(selector);
-export const $$ = (selector, ctx = document) => [...ctx.querySelectorAll(selector)];
-export const prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
